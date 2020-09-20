7944c6f9b0948288fe3ed9e2f34f98ec3475de7d
er-nutella <71012600+er-nutella@users.noreply.github.com>
2020-09-20 18:55:46 +0200

    1.7

---
+++ Platform/Assets/Scripts/ObjectMov/GiroAntiO.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiroAntiO : MonoBehaviour
+{
+    public float Rotazione = 50.0f;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * Rotazione * Time.deltaTime);
+    }
+}
---
+++ Platform/Assets/Scripts/ObjectMov/GiroOrario.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiroOrario : MonoBehaviour
+{
+    public float rotazione = 50.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.back * rotazione * Time.deltaTime);
+    }
+}
