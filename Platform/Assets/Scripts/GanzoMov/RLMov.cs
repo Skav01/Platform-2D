619478b920ee4a7ae2718c8ca496f54da3731f25
er-nutella <71012600+er-nutella@users.noreply.github.com>
2020-09-22 02:10:20 +0200

    mod3.3

---
+++ Platform/Assets/Scripts/GanzoMov/RLMov.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RLMov : MonoBehaviour
+{
+    public float velox=10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * velox * Time.deltaTime);
+       
+        
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.transform.CompareTag("Change"))
+        {
+            Debug.Log("Preso");
+            //velox *= -1;
+ 
+        }
+    }
+}
