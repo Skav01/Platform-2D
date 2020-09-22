08c28760c593ff39964c1d78a48c93a210b99701
er-nutella <71012600+er-nutella@users.noreply.github.com>
2020-09-22 20:30:21 +0200

    mod3.7

---
+++ Platform/Assets/Scripts/Destroy/DMuri.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DMuri : MonoBehaviour
+{
+    GameObject Muri;
+    GameObject bottone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Muri = GameObject.FindWithTag("Muri");
+        bottone = GameObject.FindWithTag("bottone");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.transform.CompareTag("bottone"))
+        {
+
+            DestroyObject(Muri);
+
+        }
+    }
+}
---
+++ Platform/Assets/Scripts/ObjectMov/Cover.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cover : MonoBehaviour
+{
+    public GameObject Protection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if(col.transform.CompareTag("Player"))
+        {
+            Protection.velocity = new Vector2(speed, 0);
+
+        }
+
+    }
+   
+}
