e80ea4be301a091e9b39e9ea7df3c929bc9ec505
er-nutella <71012600+er-nutella@users.noreply.github.com>
2020-09-25 01:09:29 +0200

    mod5.2

---
+++ Platform/Assets/Scripts/Destroy/enemyC.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyC : MonoBehaviour
+{
+   GameObject enemyCatenozzo;
+    GameObject bottone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemyCatenozzo = GameObject.FindWithTag("enemyC");
+        bottone = GameObject.FindWithTag("bottone");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.transform.CompareTag("bottone"))
+        {
+
+            Object.Destroy(enemyCatenozzo);
+
+        }
+    }    
+}
