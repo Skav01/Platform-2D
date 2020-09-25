e5003c4e807642a81baaae566769fc33862cdba9
er-nutella <71012600+er-nutella@users.noreply.github.com>
2020-09-25 20:37:54 +0200

    mod 5.6

---
+++ Platform/Assets/Scripts/Able&Dis/Able.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Able : MonoBehaviour
+{
+    public GameObject ObjectToAble;
+    public static bool able = false;
+    GameObject bottone;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bottone = GameObject.FindWithTag("bottone");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.transform.CompareTag("bottone"))
+            ObjectToAble.SetActive(true);
+    }
+}
