569bca71db5d5755907e989bb914f67b13a367bc
skav01 <alessandro.scavino@gmail.com>
2020-09-27 17:52:19 +0200

    mod 5.8

---
+++ Platform/Assets/Scripts/SpaceShip/protiettili.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class protiettili : MonoBehaviour
+{
+    public float speed;
+    public GameObject enemy;
+    public float z;
+    void FixedUpdate()
+    {
+        //Vector3 pos = transform.position;
+
+        //Vector3 velocity = new Vector3(speed * Time.deltaTime, 0, z);
+
+        //pos += transform.rotation * velocity;
+        //transform.position = pos;
+
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+
+        if (col.transform.CompareTag("enemy"))
+        {
+            Destroy(enemy);
+        }
+    }
+}
+
---
+++ Platform/Assets/Scripts/SpaceShip/spawnProietili.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnProietili : MonoBehaviour
+{
+    public GameObject proiettili;
+    public Transform spawnBullet;
+
+    public float spawnRate;
+    public float nextSpawn;
+
+  
+
+    int whatToSpwan;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Instantiate(quadratorosso, new Vector3(Random.Range(-10,10), Random.Range(-15,15), 0), Quaternion.identity);
+
+        if (Time.time > nextSpawn)
+        {
+            whatToSpwan = Random.Range(1, 2);
+
+            switch (whatToSpwan)
+            {
+                case 1:
+                    //Instantiate(proiettili, new Vector3(spawnBullet.position.x,spawnBullet.position.y,proiettili.transform.position.z), Quaternion.identity);
+                    Instantiate(proiettili);
+                    //Instantiate(quadratorosso, transform.position, Quaternion.identity);
+                    break;
+
+            }
+
+            nextSpawn = Time.time + spawnRate;
+
+        }
+    }
+
+}
