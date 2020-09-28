42e15c4ae9c5dc7d08e68b7e0418ffda803ee2e2
skav01 <alessandro.scavino@gmail.com>
2020-09-28 13:31:56 +0200

    mod 6.8

---
+++ Platform/Assets/Scripts/Pause/Pause.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    public GameObject PauseUI;
+    public void stop()
+    {
+        PauseUI.SetActive(true);
+        Time.timeScale = 0f;
+    }
+}
